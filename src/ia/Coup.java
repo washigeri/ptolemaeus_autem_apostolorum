5414818f05b065b40142d6bbed227ffe59691856
washigeri <malek.elouerghi@gmail.com>
2017-02-18 01:19:47 +0100

    "projet"

---
+++ src/ia/Coup.java
@@ -0,0 +1,33 @@
+package ia;
+
+import client.InfluenceCell;
+
+import java.util.ArrayList;
+
+/**
+ * Created by malek on 18/02/17.
+ */
+public class Coup {
+    InfluenceCell attaquant;
+    InfluenceCell victime;
+    int score;
+
+    public Coup(InfluenceCell attq, InfluenceCell def){
+        this.attaquant = attq;
+        this.victime = def;
+        this.score = attq.getUnitsCount() - def.getUnitsCount();
+    }
+
+    public static Coup max_coup(ArrayList<Coup> list){
+        Coup res = null;
+        if(list != null){
+            res = list.get(0);
+            for( Coup c : list){
+                if( c.score > res.score ){
+                    res = c;
+                }
+            }
+        }
+        return  res;
+    }
+}
